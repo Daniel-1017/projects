c049b012ba68e7c4d95aef1cc25fc94c70d26ebb
Daniel-17 <frimudaniel17@gmail.com>
2022-10-13 20:52:23 +0200

    header component

--- react-projects/responsive-portfolio-website/src/components/header/Header.jsx
+++ react-projects/responsive-portfolio-website/src/components/header/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
 import "./Header.css";
 
+import ME from "../../assets/me.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Hajia Bintu</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
