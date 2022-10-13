1bc78f5aad430c72d1f2902c93a886f830a0e5c5
Daniel-17 <frimudaniel17@gmail.com>
2022-10-13 20:00:59 +0200

    creating all components

--- react-projects/responsive-portfolio-website/src/App.jsx
+++ react-projects/responsive-portfolio-website/src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Experience from "./components/experience/Experience";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      <Header />
+      <Nav />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+    </Fragment>
+  );
 };
 
 export default App;
