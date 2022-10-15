e414f07008bede9f6efb7761fef8e4af14ca313f
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 15:35:44 +0200

    about page

--- react-projects/portfolio-website-advanced-animation/src/App.jsx
+++ react-projects/portfolio-website-advanced-animation/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import Layout from "./components/Layout"
+import Layout from "./components/Layout/Layout"
 import Home from "./components/Home/Home"
+import About from "./components/About/About"
 import "./App.scss"
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />} />
       <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
     </Routes>
   )
 }
