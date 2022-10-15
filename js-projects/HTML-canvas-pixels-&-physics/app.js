70ac7a0de1db97737934a5b421b129c886f64e20
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 22:40:48 +0200

    classes and particle system

--- js-projects/HTML-canvas-pixels-&-physics/app.js
+++ js-projects/HTML-canvas-pixels-&-physics/app.js
@@ -3,4 +3,16 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  class Particle {
+    constructor() {
+      this.x = 0;
+      this.y = 0;
+      this.size = 3;
+    }
+  }
+
+  class Effect {}
+
+  function animate() {}
 });
