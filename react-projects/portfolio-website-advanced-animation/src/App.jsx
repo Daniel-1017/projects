3e17e0365766b3f6f5ba67d35f905720d65e5a0a
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 13:44:51 +0200

    home page

--- react-projects/portfolio-website-advanced-animation/src/App.jsx
+++ react-projects/portfolio-website-advanced-animation/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout"
+import Home from "./components/Home/Home"
 import "./App.scss"
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />} />
+      <Route index element={<Home />} />
     </Routes>
   )
 }
