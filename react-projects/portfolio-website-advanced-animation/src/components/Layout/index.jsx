493f88cd407d56fcaccaaa339034f31e7a96b953
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 13:27:34 +0200

    building site layout

--- react-projects/portfolio-website-advanced-animation/src/components/Layout/index.jsx
+++ react-projects/portfolio-website-advanced-animation/src/components/Layout/index.jsx
@@ -1,12 +1,21 @@
-import { Fragment } from "react"
+import { Outlet } from "react-router-dom"
 import Sidebar from "../Sidebar/Sidebar"
 import "./index.scss"
 
 const Layout = () => {
   return (
-    <Fragment>
+    <div className="App">
       <Sidebar />
-    </Fragment>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
   )
 }
 
