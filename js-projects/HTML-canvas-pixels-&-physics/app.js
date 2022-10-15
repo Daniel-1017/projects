748f4e50c0ef4b86a5162820b54777b67c44bdd8
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 22:55:41 +0200

    Effect class

--- js-projects/HTML-canvas-pixels-&-physics/app.js
+++ js-projects/HTML-canvas-pixels-&-physics/app.js
@@ -4,15 +4,38 @@ window.addEventListener("load", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
+  const image1 = document.getElementById("image1");
+
   class Particle {
     constructor() {
       this.x = 0;
       this.y = 0;
-      this.size = 3;
+      this.size = 30;
+    }
+
+    draw() {
+      ctx.fillRect(this.x, this.y, this.size, this.size);
     }
   }
 
-  class Effect {}
+  class Effect {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+      this.particlesArray = [];
+    }
 
+    init() {
+      this.particlesArray.push(new Particle());
+    }
+
+    draw() {
+      this.particlesArray.forEach((particle) => particle.draw());
+    }
+  }
+
+  const effect = new Effect(canvas.width, canvas.height);
+  effect.init();
+  effect.draw();
   function animate() {}
 });
