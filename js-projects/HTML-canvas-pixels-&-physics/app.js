ff1bcad5f10edae1890c63296be96e82e32b1b91
Daniel-17 <frimudaniel17@gmail.com>
2022-10-15 23:48:15 +0200

    extracting coordinates and colors from row pixel data

--- js-projects/HTML-canvas-pixels-&-physics/app.js
+++ js-projects/HTML-canvas-pixels-&-physics/app.js
@@ -5,10 +5,11 @@ window.addEventListener("load", function () {
   canvas.height = window.innerHeight;
 
   class Particle {
-    constructor(effect) {
+    constructor(effect, x, y, color) {
       this.effect = effect;
-      this.x = Math.random() * this.effect.width;
-      this.y = Math.random() * this.effect.height;
+      this.originX = Math.floor(x);
+      this.originY = Math.floor(y);
+      this.color = color;
       this.size = 10;
       this.vx = Math.random() * 2 - 1;
       this.vy = Math.random() * 2 - 1;
@@ -34,12 +35,26 @@ window.addEventListener("load", function () {
       this.centerY = this.height * 0.5;
       this.x = this.centerX - this.image.width * 0.5;
       this.y = this.centerY - this.image.height * 0.5;
+      this.gap = 5;
     }
 
     init(context) {
       context.drawImage(this.image, this.x, this.y);
-      const pixels = context.getImageData(0, 0, this.width, this.height);
-      console.log(pixels);
+      const pixels = context.getImageData(0, 0, this.width, this.height).data;
+      for (let y = 0; y < this.height; y += this.gap) {
+        for (let x = 0; x < this.width; x += this.gap) {
+          const index = (y * this.width + x) * 4;
+          const red = pixels[index];
+          const green = pixels[index + 1];
+          const blue = pixels[index + 2];
+          const alpha = pixels[index + 3];
+          const color = `rgb(${red}, ${green}, ${blue})`;
+
+          if (alpha > 0) {
+            this.particlesArray.push(new Particle(this, x, y, color));
+          }
+        }
+      }
     }
 
     draw(context) {
