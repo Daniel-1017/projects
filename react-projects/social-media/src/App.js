c44105c4292826c55b917c590295ad842f0e20dc
Daniel-17 <frimudaniel17@gmail.com>
2022-10-30 18:32:16 +0100

    navigation between login and register page + outlet

--- react-projects/social-media/src/App.js
+++ react-projects/social-media/src/App.js
@@ -1,10 +1,55 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Navbar from "./components/navbar/Navbar";
+import Leftbar from "./components/leftbar/Leftbar";
+import Rightbar from "./components/rightbar/Rightbar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Leftbar />
+          <Outlet />
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 }
