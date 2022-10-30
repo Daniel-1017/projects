43da1eba4842ab662b376f2ec6ac621a8c310310
Daniel-17 <frimudaniel17@gmail.com>
2022-10-30 18:11:28 +0100

    register page

---
+++ react-projects/social-media/src/pages/register/Register.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./register.scss";
+
+const Register = () => {
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ex
+            corrupti, voluptas, nam vitae deserunt, blanditiis maxime quos
+            voluptates.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="E-mail" />
+            <input type="password" placeholder="Password" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
