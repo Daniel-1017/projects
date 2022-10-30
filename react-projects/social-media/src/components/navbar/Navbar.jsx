493f8a9950146b5f4c69f86889acb69ab76cfcad
Daniel-17 <frimudaniel17@gmail.com>
2022-10-30 19:22:16 +0100

    navbar

--- react-projects/social-media/src/components/navbar/Navbar.jsx
+++ react-projects/social-media/src/components/navbar/Navbar.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import {
+  HomeOutlined,
+  DarkModeOutlined,
+  WbSunnyOutlined,
+  GridViewOutlined,
+  NotificationsOutlined,
+  EmailOutlined,
+  PersonOutline,
+  SearchOutlined,
+} from "@mui/icons-material";
+
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/home" style={{ textDecoration: "none" }}>
+          <span>lamasocial</span>
+        </Link>
+        <HomeOutlined />
+        <DarkModeOutlined />
+        <GridViewOutlined />
+        <div className="search">
+          <SearchOutlined />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutline />
+        <EmailOutlined />
+        <NotificationsOutlined />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt="profile image"
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
