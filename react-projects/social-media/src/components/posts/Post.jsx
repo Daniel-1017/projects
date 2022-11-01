d4a2b72dfe08b84c0fb07d61f9ca5a0b5ccf685d
Daniel-17 <frimudaniel17@gmail.com>
2022-11-01 18:43:03 +0100

    user comments

--- react-projects/social-media/src/components/posts/Post.jsx
+++ react-projects/social-media/src/components/posts/Post.jsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import "./post.scss";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import "./post.scss";
 
 const Post = ({ post }) => {
   const liked = false;
+  const [commentOpen, setCommentOpen] = useState(false);
 
   return (
     <div className="post">
@@ -30,14 +32,17 @@ const Post = ({ post }) => {
         </div>
         <div className="content">
           <p>{post.desc}</p>
-          <img src={post.img} alt="post pic" />
+          {post.img && <img src={post.img} alt="post pic" />}
         </div>
         <div className="info">
           <div className="item">
             {!liked ? <FavoriteBorderOutlinedIcon /> : <FavoriteOutlinedIcon />}
             12 Likes
           </div>
-          <div className="item">
+          <div
+            className="item"
+            onClick={() => setCommentOpen((prevState) => !prevState)}
+          >
             <TextsmsOutlinedIcon />
             10 Comments
           </div>
@@ -46,6 +51,7 @@ const Post = ({ post }) => {
             Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   );
