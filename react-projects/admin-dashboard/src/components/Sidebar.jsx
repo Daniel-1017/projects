86a1320147349097a36db5b4f85e0b2b9252f922
Daniel-17 <frimudaniel17@gmail.com>
2022-11-09 15:05:16 +0100

    navbar

--- react-projects/admin-dashboard/src/components/Sidebar.jsx
+++ react-projects/admin-dashboard/src/components/Sidebar.jsx
@@ -7,10 +7,16 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
 
   const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
@@ -21,7 +27,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
@@ -29,7 +35,7 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => setActiveMenu((prev) => !prev)}
+                onClick={() => setActiveMenu(!activeMenu)}
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
               >
                 <MdOutlineCancel />
@@ -43,8 +49,8 @@ const Sidebar = () => {
                 {link.links.map((item) => (
                   <NavLink
                     to={`/${item.name}`}
+                    onClick={handleCloseSidebar}
                     key={item.name}
-                    onClick={() => {}}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
